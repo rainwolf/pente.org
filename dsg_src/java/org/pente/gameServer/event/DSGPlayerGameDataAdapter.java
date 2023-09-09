de4071d8f6c020796bf9c493f199642095014f40
rainwolf <rainwolf@submanifold.be>
2023-09-09 12:23:24 +0800

    json fix for dsgplayergamedata

--- dsg_src/java/org/pente/gameServer/event/DSGPlayerGameDataAdapter.java
+++ dsg_src/java/org/pente/gameServer/event/DSGPlayerGameDataAdapter.java
@@ -1,20 +1,34 @@
 package org.pente.gameServer.event;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
+import com.google.gson.*;
 import org.pente.gameServer.core.DSGPlayerGameData;
 import org.pente.gameServer.core.SimpleDSGPlayerGameData;
 
 import java.lang.reflect.Type;
 
-public class DSGPlayerGameDataAdapter implements JsonDeserializer<DSGPlayerGameData> {
+public class DSGPlayerGameDataAdapter implements JsonDeserializer<DSGPlayerGameData>, JsonSerializer<DSGPlayerGameData> {
 
     @Override
     public DSGPlayerGameData deserialize(JsonElement json, Type typeOfT,
                                          JsonDeserializationContext context) throws JsonParseException {
 
         return context.deserialize(json, SimpleDSGPlayerGameData.class);
+    }
+
+    @Override
+    public JsonElement serialize(DSGPlayerGameData dsgPlayerGameData, Type type, JsonSerializationContext jsonSerializationContext) {
+        JsonObject dsgPlayerGameDataObject = new JsonObject();
+
+        dsgPlayerGameDataObject.add("playerID", new JsonPrimitive(dsgPlayerGameData.getPlayerID()));
+        dsgPlayerGameDataObject.add("game", new JsonPrimitive(dsgPlayerGameData.getGame()));
+        dsgPlayerGameDataObject.add("wins", new JsonPrimitive(dsgPlayerGameData.getWins()));
+        dsgPlayerGameDataObject.add("losses", new JsonPrimitive(dsgPlayerGameData.getLosses()));
+        dsgPlayerGameDataObject.add("draws", new JsonPrimitive(dsgPlayerGameData.getDraws()));
+        dsgPlayerGameDataObject.add("rating", new JsonPrimitive(dsgPlayerGameData.getRating()));
+        dsgPlayerGameDataObject.add("streak", new JsonPrimitive(dsgPlayerGameData.getStreak()));
+        dsgPlayerGameDataObject.add("lastGameDate", new JsonPrimitive(dsgPlayerGameData.getLastGameDate().toString()));
+        dsgPlayerGameDataObject.add("tourneyWinner", new JsonPrimitive(dsgPlayerGameData.getTourneyWinner()));
+        dsgPlayerGameDataObject.add("computer", new JsonPrimitive(dsgPlayerGameData.getComputer()));
+        return dsgPlayerGameDataObject;
     }
 }
