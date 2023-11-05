b4bd97c9692a994e7e99ba7758d255fce72b1ba3
rainwolf <rainwolf@submanifold.be>
2023-11-05 15:56:07 +0800

    Update build_and_deploy.sh

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -3,18 +3,22 @@
 docker system prune -af
 DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose build
 
-docker save auto_ssh | bzip2 | pv | ssh debian@51.79.159.111 docker load && docker image prune -f
-docker save dsg_sql | bzip2 | pv | ssh debian@51.79.159.111 docker load && docker image prune -f
+docker save auto_ssh | bzip2 | pv | ssh debian@51.79.159.111 docker load
+docker save dsg_sql | bzip2 | pv | ssh debian@51.79.159.111 docker load
 
-docker save pente.org | bzip2 | pv | ssh rainwolf@pente.org docker load && docker image prune -f
-docker save auto_ssh | bzip2 | pv | ssh rainwolf@pente.org docker load && docker image prune -f
-docker save dsg_sql | bzip2 | pv | ssh rainwolf@pente.org docker load && docker image prune -f
-docker save dsg_mail | bzip2 | pv | ssh rainwolf@pente.org docker load && docker image prune -f
+docker save pente.org | bzip2 | pv | ssh rainwolf@pente.org docker load
+docker save auto_ssh | bzip2 | pv | ssh rainwolf@pente.org docker load
+docker save dsg_sql | bzip2 | pv | ssh rainwolf@pente.org docker load
+docker save dsg_mail | bzip2 | pv | ssh rainwolf@pente.org docker load
 
-docker save pente.org | bzip2 | pv | ssh rainwolf@development.pente.org docker load && docker image prune -f
-docker save auto_ssh | bzip2 | pv | ssh rainwolf@development.pente.org docker load && docker image prune -f
-docker save dsg_sql | bzip2 | pv | ssh rainwolf@development.pente.org docker load && docker image prune -f
-docker save dsg_mail | bzip2 | pv | ssh rainwolf@development.pente.org docker load && docker image prune -f
+docker save pente.org | bzip2 | pv | ssh rainwolf@development.pente.org docker load
+docker save auto_ssh | bzip2 | pv | ssh rainwolf@development.pente.org docker load
+docker save dsg_sql | bzip2 | pv | ssh rainwolf@development.pente.org docker load
+docker save dsg_mail | bzip2 | pv | ssh rainwolf@development.pente.org docker load
+
+ssh debian@51.79.159.111 docker image prune -f
+ssh rainwolf@pente.org docker image prune -f
+ssh rainwolf@development.pente.org docker image prune -f
 
 docker system prune -af
 docker compose build
