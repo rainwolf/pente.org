16ce45d2c6f17971caeb26ab01232b81c508c47f
rainwolf <rainwolf@submanifold.be>
2023-11-06 21:21:43 +0800

    changing the root pw on startup

--- dockerMain/custom-entrypoint.sh
+++ dockerMain/custom-entrypoint.sh
@@ -12,6 +12,25 @@ docker_setup_env "$@"
 
 if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
   echo "Database does not exist; not running always-run hooks"
+elif test -n "$(shopt -s nullglob; echo /update-root-initdb.d/*)"; then
+  # Database exists; run always-run hooks if they exist
+  mysql_note "Starting temporary server"
+  docker_temp_server_start "$@" --skip-grant-tables --skip-networking
+  mysql_note "Temporary server started."
+
+  docker_process_init_files /update-root-initdb.d/*
+
+  mysql_note "Stopping temporary server"
+  docker_temp_server_stop
+  mysql_note "Temporary server stopped"
+
+  echo
+  mysql_note "MySQL init process done. Ready for start up."
+  echo
+fi
+
+if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+  echo "Database does not exist; not running always-run hooks"
 elif test -n "$(shopt -s nullglob; echo /always-initdb.d/*)"; then
   # Database exists; run always-run hooks if they exist
   mysql_note "Starting temporary server"
