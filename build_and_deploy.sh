152d64ec2860b499e4cf86455555e8293f0486e2
rainwolf <rainwolf@submanifold.be>
2023-11-19 10:28:11 +0800

    Update build_and_deploy.sh

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,24 +1,63 @@
 #!/usr/bin/env bash
 
+read -a array <<< "$@"
+
 docker system prune -af
-DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose build
 
-docker save auto_ssh | bzip2 | pv | ssh debian@51.79.159.111 docker load
-docker save dsg_sql | bzip2 | pv | ssh debian@51.79.159.111 docker load
+if [[ ${#array[@]} -eq 0 ]]
+then
+  echo "Building everything for linux/amd64"
+  DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose build
+else
+  for target in "${array[@]}"
+  do
+    echo "Building ${target} for linux/amd64"
+    DOCKER_DEFAULT_PLATFORM=linux/amd64 docker compose build "${array[index]}"
+  done
+fi
 
-docker save pente.org | bzip2 | pv | ssh rainwolf@pente.org docker load
-docker save auto_ssh | bzip2 | pv | ssh rainwolf@pente.org docker load
-docker save dsg_sql | bzip2 | pv | ssh rainwolf@pente.org docker load
-docker save dsg_mail | bzip2 | pv | ssh rainwolf@pente.org docker load
 
-docker save pente.org | bzip2 | pv | ssh rainwolf@development.pente.org docker load
-docker save auto_ssh | bzip2 | pv | ssh rainwolf@development.pente.org docker load
-docker save dsg_sql | bzip2 | pv | ssh rainwolf@development.pente.org docker load
-docker save dsg_mail | bzip2 | pv | ssh rainwolf@development.pente.org docker load
+if [[ ${#array[@]} -eq 0 || ${array[@]} =~ "main_auto_ssh" ]]
+then
+  for target in "debian@51.79.69.199" "debian@51.79.159.111" "rainwolf@development.pente.org"
+  do
+    echo "Pushing auto_ssh to ${target}"
+    docker save auto_ssh | bzip2 | pv | "${target}" docker load
+  done
+fi
 
-ssh debian@51.79.159.111 docker image prune -f
-ssh rainwolf@pente.org docker image prune -f
-ssh rainwolf@development.pente.org docker image prune -f
+if [[ ${#array[@]} -eq 0 || ${array[@]} =~ "main_db" ]]
+then
+  for target in "debian@51.79.69.199" "debian@51.79.159.111" "rainwolf@development.pente.org"
+  do
+    echo "Pushing dsg_sql to ${target}"
+    docker save dsg_sql | bzip2 | pv | "${target}" docker load
+  done
+fi
+
+if [[ ${#array[@]} -eq 0 || ${array[@]} =~ "pente_mail" ]]
+then
+  for target in "debian@51.79.69.199" "rainwolf@development.pente.org"
+  do
+    echo "Pushing dsg_mail to ${target}"
+    docker save dsg_mail | bzip2 | pv | "${target}" docker load
+  done
+fi
+
+if [[ ${#array[@]} -eq 0 || ${array[@]} =~ "pente.org" ]]
+then
+  for target in "debian@51.79.69.199" "rainwolf@development.pente.org"
+  do
+    echo "Pushing pente.org to ${target}"
+    docker save pente.org | bzip2 | pv | "${target}" docker load
+  done
+fi
+
+for target in "debian@51.79.69.199" "debian@51.79.159.111" "rainwolf@development.pente.org" "rainwolf@pente.org"
+do
+  echo "Cleaning up ${target}"
+  ssh "${target}" docker image prune -f
+done
 
 docker system prune -af
 docker compose build
