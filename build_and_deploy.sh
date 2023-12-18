87f1f1ac3f0618c5873a7bf3f49248f85ff7d0b3
rainwolf <rainwolf@submanifold.be>
2023-12-18 15:10:54 +0100

    enhance build/deploy script

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 clear; printf '\e[3J';
+
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
 rsync -vurtd --exclude-from exclude_compile.txt --stats --progress dsg_src/java/ deploy/
 
 read -a array <<< "$@"
 
 docker system prune -af
-
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 if [[ ${#array[@]} -eq 0 ]]
 then
@@ -39,50 +40,27 @@ else
   done
 fi
 
-if [[ ${#array[@]} -eq 0 || ${array[@]} =~ "main_auto_ssh" ]]
-then
-  for target in "debian@pente.org" "debian@wire.submanifold.be"
-  do
-    echo "Pushing auto_ssh to ${target}"
-    docker save auto_ssh | bzip2 | pv | ssh "${target}" docker load
-  done
-fi
+read -a images_main <<< $(docker compose -f docker-compose.yml config --images | sort)
+read -a images_replica <<< $(docker compose -f docker-compose-replica.yml config --images | sort)
+read -a built_images <<< $(docker images --format json | jq .Repository | sed 's/\"//g' | sort)
+images_main_combined=( "${images_main[@]} ${built_images[@]}" )
+images_main_push=$(echo "${images_main_combined[@]}" | xargs -n1 | sort | uniq -d | xargs)
+images_replica_combined=( "${images_replica[@]} ${built_images[@]}" )
+images_replica_push=$(echo "${images_replica_combined[@]}" | xargs -n1 | sort | uniq -d | xargs)
 
-if [[ ${#array[@]} -eq 0 || ${array[@]} =~ "main_db" ]]
-then
-  for target in "debian@pente.org" "debian@wire.submanifold.be"
-  do
-    echo "Pushing dsg_sql to ${target}"
-    docker save dsg_sql | bzip2 | pv | ssh "${target}" docker load
-  done
-fi
+target="debian@pente.org"
+for image in ${images_main_push[@]}
+do
+  echo "Pushing ${image} to ${target}"
+  docker save "${image}" | bzip2 | pv | ssh "${target}" docker load
+done
+target="debian@wire.submanifold.be"
+for image in ${images_replica_push[@]}
+do
+  echo "Pushing ${image} to ${target}"
+  docker save "${image}" | bzip2 | pv | ssh "${target}" docker load
+done
 
-if [[ ${#array[@]} -eq 0 || ${array[@]} =~ "pentai" ]]
-then
-  for target in "debian@wire.submanifold.be"
-  do
-    echo "Pushing dsg_pentai to ${target}"
-    docker save dsg_pentai | bzip2 | pv | ssh "${target}" docker load
-  done
-fi
-
-if [[ ${#array[@]} -eq 0 || ${array[@]} =~ "pente_mail" ]]
-then
-  for target in "debian@pente.org"
-  do
-    echo "Pushing dsg_mail to ${target}"
-    docker save dsg_mail | bzip2 | pv | ssh "${target}" docker load
-  done
-fi
-
-if [[ ${#array[@]} -eq 0 || ${array[@]} =~ "pente.org" ]]
-then
-  for target in "debian@pente.org"
-  do
-    echo "Pushing pente.org to ${target}"
-    docker save pente.org | bzip2 | pv | ssh "${target}" docker load
-  done
-fi
 
 # restart the containers with new images
 ssh debian@pente.org docker compose -f docker-compose.yml up -d
