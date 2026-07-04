5a1e37ba7953c95e7e5b52655cb68822fa48446f
rainwolf <walied.othman@gmail.com>
2026-07-04 13:45:39 +0700

    TB tournament: resolve unplayed match via original seats (covers renju)

--- dsg_src/java/org/pente/turnBased/CacheTBStorer.java
+++ dsg_src/java/org/pente/turnBased/CacheTBStorer.java
@@ -1123,22 +1123,12 @@ public class CacheTBStorer implements TBGameStorer, TourneyListener {
                     kothStorer.updatePlayerLastGameDate(game.getGame(), winnerData.getPlayerID());
                     kothStorer.updatePlayerLastGameDate(game.getGame(), loserData.getPlayerID());
                 } else if (game.getEventId() != getEventId(game.getGame())) {
-                    TourneyMatch tourneyMatch = null;
-                    if ((game.getGame() == GridStateFactory.TB_DPENTE ||
-                            game.getGame() == GridStateFactory.TB_DKERYO ||
-                            game.getGame() == GridStateFactory.TB_SWAP2PENTE ||
-                            game.getGame() == GridStateFactory.TB_SWAP2KERYO) && game.didDPenteSwap()) {
-                        tourneyMatch = tourneyStorer.getUnplayedMatch(game.getPlayer2Pid(), game.getPlayer1Pid(), game.getEventId());
-                    } else {
-                        tourneyMatch = tourneyStorer.getUnplayedMatch(game.getPlayer1Pid(), game.getPlayer2Pid(), game.getEventId());
-                    }
+                    TourneyMatch tourneyMatch = tourneyStorer.getUnplayedMatch(
+                            game.getOriginalPlayer1Pid(), game.getOriginalPlayer2Pid(), game.getEventId());
                     if (tourneyMatch != null) {
                         tourneyMatch.setGid(game.getGid());
                         int winner = game.getWinner();
-                        if ((game.getGame() == GridStateFactory.TB_DPENTE ||
-                                game.getGame() == GridStateFactory.TB_DKERYO ||
-                                game.getGame() == GridStateFactory.TB_SWAP2PENTE ||
-                                game.getGame() == GridStateFactory.TB_SWAP2KERYO) && game.didDPenteSwap()) {
+                        if (game.seatsSwapped()) {
                             winner = 3 - winner;
                         }
                         tourneyMatch.setResult(winner);
